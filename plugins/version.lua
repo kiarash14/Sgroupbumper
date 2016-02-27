e7474f5d410e8a4c7a3512d482cf579e6d8cfca9
kiarash14 <omidbakhsh22@gmail.com>
2016-02-27 08:59:34 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,8 @@
 do
 
 function run(msg, matches)
-  return 'Telegram NOD32-BOT Bot '.. VERSION .. [[ 
-  Checkout https://github.com/BH-YAGHI/NOD32-SUPER.git
-  GNU GPL v2 license.]]
+  return 'Telegram Bumper Bot '.. VERSION .. [[ 
+admin: @kiarash_gh14]]
 end
 
 return {
